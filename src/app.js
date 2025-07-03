c4fc20a3d2852943eaf886c375578c3333699dca
harshit bagaria <harshit.bagaria.cer21@iitbhu.ac.in>
2025-07-03 16:59:28 +0530

    cros origin updated

--- src/app.js
+++ src/app.js
@@ -1,16 +1,20 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import dotenv from  "dotenv";
+dotenv.config();
 
 const app = express();
 
 // --- 1. CORE MIDDLEWARES ---
 // These should come first to process every incoming request.
 
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
 
 // Middleware to parse JSON bodies
 app.use(express.json({ limit: "16kb" }));
