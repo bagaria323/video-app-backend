33463496aff55df8eebfb534963bd38f80d4c00f
harshit bagaria <harshit.bagaria.cer21@iitbhu.ac.in>
2025-07-03 16:31:17 +0530

    db file improved

--- src/db/index.js
+++ src/db/index.js
@@ -7,11 +7,13 @@ dotenv.config()
 
  const dbcall = async () => { 
     try {
+         console.log("Attempting to connect to MongoDB...");
         const connectioninstance = await mongoose.connect(process.env.MONGODB_URI) 
+        console.log(`\n ✅ SUCCESS: MongoDB connected! DB HOST: ${process.env.MONGODB_URI}`);
 
 console.log(`connection host : ${connectioninstance.connection.host}`);
     } catch (error) {
-         console.log(`mongo error ` , error ); 
+         console.log(`FAILED to connect to MongoDB due to mongo error ` , error ); 
          process.exit(1) ;
     }
  }
